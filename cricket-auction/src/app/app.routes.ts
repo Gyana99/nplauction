2477387baf677a0cee36e9fba60669008a8de24d
Gyana ranjan pati <Gyana ranjan pati>
2025-12-11 09:28:46 +0530

    first commit

---
+++ cricket-auction/src/app/app.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+import { PlayerRegistrationComponent } from './player-registration/player-registration.component';
+
+export const routes: Routes = [
+    {
+        path: '', component: PlayerRegistrationComponent
+    }
+];
--- nplwebsite/src/app/app-routing.module.ts
+++ nplwebsite/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PlayerRegistrationComponent } from './player-registration/player-registration.component';
+PlayerRegistrationComponent
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',component:PlayerRegistrationComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
