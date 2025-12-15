b32300ef01d1f877e848d8a95e69a4e4d2b4cee2
Gyana99 <gyanapati6@gmail.com>
2025-12-15 11:35:59 +0530

    gyana

--- npladmin/src/app/admin/admindasbord/admindasbord.component.ts
+++ npladmin/src/app/admin/admindasbord/admindasbord.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { CommonService } from 'src/app/services/common.service';
+
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-admindasbord',
   templateUrl: './admindasbord.component.html',
-  styleUrls: ['./admindasbord.component.scss']
+  styleUrls: ['./admindasbord.component.scss'],
 })
 export class AdmindasbordComponent {
+    teams: any=[];
+    path :any = environment.apiUrl+'storage/uploads/'
+ ngOnInit(): void {
+  this.loadTeams();
+ }
+constructor(private CS: CommonService) { }
 
+    loadTeams() {
+    this.CS.viewTeam().subscribe((res: any) => {
+      if (res.status) {
+        this.teams = res.data;
+      }
+    });
+    // API CALL HERE
+    // this.teams = [
+    //   { id: 1, team_name: 'Warriors', short_name: 'WAR', amount: 5000, status: 1 }
+    // ];
+  }
 }
